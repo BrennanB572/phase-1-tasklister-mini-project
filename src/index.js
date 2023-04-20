f7e44ab0aaf28689060f2c0138f30a36d9c4bec0
Brennan Bradley <Brennanb572@gmail.com>
2023-04-19 22:21:43 -0700

    project complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+      let form = document.querySelector('form')
+      form.addEventListener('submit', (e) => { e.preventDefault()
+        handleTask(e.target.new_task_description.value)
+        form.reset()
+      })
+})
+
+function handleTask(task){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'x'
+  p.textContent = `${task} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+}
+
+function deleteTask(e){
+     e.target.parentNode.remove()
+}
